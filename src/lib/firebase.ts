33a34e84f585344a5267e4cb58b588d892bca4a5
ZaneMasters <angel.pro98@gmail.com>
2026-05-08 22:25:46 -0500

    deploy

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -26,3 +26,6 @@ export const storage = getStorage(app);
 // Initialize Firebase Authentication and provider
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+
+//hoaksdasd
